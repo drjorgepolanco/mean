cfb612123e1fa10ee0498518df7e4c90e411a18b
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-22 13:36:33 -0500

    Posts are now persisting, stored in db

--- flapper_news/public/javascripts/angularApp.js
+++ flapper_news/public/javascripts/angularApp.js
@@ -29,18 +29,18 @@ app.factory('posts', ['$http', function($http) {
   var o = {
     posts: []
   };
-
+  // Display all posts
   o.getAll = function() {
     return $http.get('/posts').success(function(data) {
       angular.copy(data, o.posts);
     });
   };
-
-  o.create = function() {
+  // Create new Post
+  o.create = function(post) {
     return $http.post('/posts', post).success(function(data) {
       o.posts.push(data);
     });
-  }
+  };
 
   return o;
 }]);
@@ -50,24 +50,13 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
   $scope.test = 'Hello World!';
   
   $scope.posts = posts.posts;
-  // $scope.posts = [
-  //   { title: 'post 1', upvotes: 5 },
-  //   { title: 'post 2', upvotes: 2 },
-  //   { title: 'post 3', upvotes: 15 },
-  //   { title: 'post 4', upvotes: 9 },
-  //   { title: 'post 5', upvotes: 4 }
-  // ];
 
+  // Save posts to the server
   $scope.addPost = function() {
-    if (!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({ 
-      title: $scope.title, 
+    if ($scope.title === '') { return; }
+    posts.create({
+      title: $scope.title,
       link: $scope.link,
-      upvotes: 0,
-      comments: [
-        { author: 'Joe', body: 'Cool post!', upvotes: 0 },
-        { author: 'Bob', body: 'Great idea but everything is wrong', upvotes: 0 }
-      ] 
     });
     $scope.title = '';
     $scope.link  = '';
