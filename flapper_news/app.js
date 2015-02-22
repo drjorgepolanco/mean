a6dfc8ae99be88756a9880f2627888e6e22d396f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-21 23:14:06 -0500

    Add the possibility to create and display comments on posts

--- flapper_news/app.js
+++ flapper_news/app.js
@@ -62,4 +62,32 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 
 app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
   $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if ($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
